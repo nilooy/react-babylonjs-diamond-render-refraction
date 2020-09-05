e091092b9b5e1aa4f779f8b374f154b3ec784c39
theprogboy <mdrezwanferdous@gmail.com>
2020-09-05 13:29:16 +0200

    test

---
+++ build/precache-manifest.145bc1701b7f598dab9668095d90fa0e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f513cc61dee034acf9f5d518521f9f",
+    "url": "react-babylonjs-diamond-render-refraction/index.html"
+  },
+  {
+    "revision": "0c3a3c4a9d998f8f080e",
+    "url": "react-babylonjs-diamond-render-refraction/static/css/2.fe727932.chunk.css"
+  },
+  {
+    "revision": "6b08f05865c98f0eb30c",
+    "url": "react-babylonjs-diamond-render-refraction/static/css/main.9ed815fc.chunk.css"
+  },
+  {
+    "revision": "0c3a3c4a9d998f8f080e",
+    "url": "react-babylonjs-diamond-render-refraction/static/js/2.8b64fa64.chunk.js"
+  },
+  {
+    "revision": "6b08f05865c98f0eb30c",
+    "url": "react-babylonjs-diamond-render-refraction/static/js/main.9be425cc.chunk.js"
+  },
+  {
+    "revision": "9dd6744932aa6737df6f",
+    "url": "react-babylonjs-diamond-render-refraction/static/js/runtime-main.580f8deb.js"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "react-babylonjs-diamond-render-refraction/static/media/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "react-babylonjs-diamond-render-refraction/static/media/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "react-babylonjs-diamond-render-refraction/static/media/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "react-babylonjs-diamond-render-refraction/static/media/MaterialIcons-Regular.d120c85b.ttf"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.88c0002bb1324c1d8b6ea1aafc17909b.js"
+  "react-babylonjs-diamond-render-refraction/precache-manifest.145bc1701b7f598dab9668095d90fa0e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("react-babylonjs-diamond-render-refraction/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
