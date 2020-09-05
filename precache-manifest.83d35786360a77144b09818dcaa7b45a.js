506c4df66ac0f5f8ce4270d0cf1f58c89258f420
theprogboy <mdrezwanferdous@gmail.com>
2020-09-05 13:38:42 +0200

    Updates

---
+++ precache-manifest.83d35786360a77144b09818dcaa7b45a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c3d3671af6434f5b0572c71438adfb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04da921a985af7695787",
+    "url": "/static/css/2.d038ee2e.chunk.css"
+  },
+  {
+    "revision": "55764e33dc09c5b33a78",
+    "url": "/static/css/main.9ed815fc.chunk.css"
+  },
+  {
+    "revision": "04da921a985af7695787",
+    "url": "/static/js/2.8b64fa64.chunk.js"
+  },
+  {
+    "revision": "55764e33dc09c5b33a78",
+    "url": "/static/js/main.08048ff8.chunk.js"
+  },
+  {
+    "revision": "e4e7bdefc0bea1aa357b",
+    "url": "/static/js/runtime-main.03a1446e.js"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/static/media/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/static/media/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/static/media/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/static/media/MaterialIcons-Regular.d120c85b.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "react-babylonjs-diamond-render-refraction/precache-manifest.145bc1701b7f598dab9668095d90fa0e.js"
+  "/precache-manifest.83d35786360a77144b09818dcaa7b45a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("react-babylonjs-diamond-render-refraction/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
